a5998b87939d53ae984177c38dfd195ae472cb9a
Reetesh <reetesh@devathon.com>
2023-04-24 17:31:27 +0530

    add:logo

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { RiCloseLine } from 'react-icons/ri';
-import { logo } from '../assets';
+import logo  from '../assets/logo.jpg';
 import { links } from '../assets/constants';
 import { HiLogout, HiOutlineMenu } from 'react-icons/hi';
 
@@ -11,7 +11,7 @@ const NavLinks = ({ handleClick }) => (
       <NavLink
         key={item.name}
         to={item.to}
-        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium  text-gray-400 hover:text-cyan-400"
         onClick={() => handleClick && handleClick()}
       >
         <item.icon className="w-6 h-6 mr-2" />
@@ -27,8 +27,8 @@ const Sidebar = () => {
   return (
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-        {/* <img src={logo} alt="logo" className="w-full h-14 object-contain" /> */}
-        <h2 className='text-2xl text-white font-extrabold'>Podcast</h2>
+        <img src={logo} alt="logo" className="w-24 h-24 rounded-full mx-auto object-cover" />
+        <span className='text-white font-extrabold text-center mt-2 text-xl'>Podcast</span>
         <NavLinks />
       </div>
       <div className="absolute md:hidden block top-6 right-3">
